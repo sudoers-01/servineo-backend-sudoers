06eba82365ea89cf5a371df79d38a904c8c64eb9
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-10-08 22:37:45 -0400

    Add: db connection

---
+++ src/config/db/mongoClient.ts
@@ -0,0 +1,31 @@
+import { MongoClient, Db } from 'mongodb';
+
+const uri = process.env.MONGO_URI;
+if (!uri) {
+  throw new Error('MONGO_URI environment variable is not defined');
+}
+const client: MongoClient = new MongoClient(uri);
+
+let db: Db;
+
+export async function connectDB(): Promise<Db> {
+  if (!db) {
+    try {
+      await client.connect();
+      db = client.db(process.env.DB_NAME);
+    } catch (error) {
+      console.error('Failed to connect to the database', error);
+      throw error;
+    }
+  }
+  return db;
+}
+
+export async function closeDB(): Promise<void> {
+  try {
+    await client.close();
+    console.log('MongoDB connection closed');
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+  }
+}

a76c06df6614bf56198a2ab3a5968cddca44750a
Dylan <dylancc20@gmail.com>
2025-10-11 01:12:54 -0400

    feat: add error handleling

--- src/modules/profile/profile.routes.ts
+++ src/modules/profile/profile.routes.ts
@@ -3,6 +3,6 @@ import * as ProfileController from './profile.controller';
 
 const router = Router();
 
-router.get('/profile', ProfileController.getProfileData);
+router.get('/profile/:id', ProfileController.getProfileData);
 
 export default router;

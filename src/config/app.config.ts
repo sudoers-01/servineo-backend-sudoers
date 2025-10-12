737fbc8dfa74a100d4b555d886222da823160b47
Saraqch <quispechoquesara@gmail.com>
2025-10-11 22:25:05 -0400

    fix: corrección de conexion a la bd atlas

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,29 +1,16 @@
-// src/config/app.config.ts
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const appConfig = {
-  port: process.env.PORT || 8000,
-
-  //mongoUri: process.env.MONGO_URI || 'mongodb:',
-  mongoUri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/servineo_db',
-
-  nodeEnv: process.env.NODE_ENV || 'development',
-
-  jwtSecret: process.env.JWT_SECRET || 'servineo-jwt-secret-super-seguro-2024',
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '24h',
-
-  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-
-  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000',
-  backendUrl: process.env.BACKEND_URL || 'http://localhost:8000',
+export const ENV = {
+  MONGO_USER: process.env.MONGO_USER,
+  MONGO_PASS: process.env.MONGO_PASS,
+  MONGO_HOST: process.env.MONGO_HOST,
+  MONGO_DB: process.env.MONGO_DB,
+  MONGO_URI: process.env.MONGO_URI,
 };
 
 export const appConfig = {
   mongoUri:
     ENV.MONGO_URI ||
     `mongodb+srv://${encodeURIComponent(ENV.MONGO_USER!)}:${encodeURIComponent(
-      ENV.MONGO_PASS!
+      ENV.MONGO_PASS!,
     )}@${ENV.MONGO_HOST}/${ENV.MONGO_DB}?retryWrites=true&w=majority&appName=${ENV.MONGO_DB}`,
   // ... otras configuraciones
 };

0394c42cf190f2bba7f34705bc9a42487e31a134
LucianaBarrosoF <202200291@est.umss.edu>
2025-10-12 00:01:33 -0400

    feat(server): Añadir ruta de ubicación al servidor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import googleRouter from "./modules/controlC/google/routes";
+import ubicacionRouter from "./modules/controlC/ubicacion/routes";
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 
 app.use("/api/controlC/google", googleRouter);
+app.use("/api/controlC/ubicacion", ubicacionRouter);
 
 app.listen(8000, () => console.log("Servidor corriendo en puerto 8000"));

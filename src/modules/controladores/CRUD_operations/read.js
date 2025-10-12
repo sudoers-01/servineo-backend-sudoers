74df10caa2c5ac107883b8d79c1f059baf64e8f9
gg1700 <gabrielcamacho1700@gmail.com>
2025-10-12 18:40:38 -0400

    Another Query Appointment Correction.

--- src/modules/controladores/CRUD_operations/read.js
+++ src/modules/controladores/CRUD_operations/read.js
@@ -75,14 +75,14 @@ async function get_all_appointments(){
     }
 }
 
-async function get_schedules_by_fixer_month(fixer_id, requester_id, month){
+async function get_all_requester_schedules_by_fixer_month(fixer_id, requester_id, month){
     try{
+        await set_db_connection();
         const current_date = new Date(Date.now());
         const current_year = current_date.getFullYear();
         const target_month = month - 1;
-        const start_date = new Date(2024, target_month, 1);
-        const finish_date = new Date(2024, month, 0, 23, 59, 59);
-        await set_db_connection();
+        const start_date = new Date(current_year, target_month, 1);
+        const finish_date = new Date(current_year, month, 0, 23, 59, 59);
         const appointment = await Appointment.find({
             id_fixer: fixer_id, 
             id_requester: {$ne: requester_id},
@@ -103,22 +103,58 @@ async function get_schedules_by_fixer_month(fixer_id, requester_id, month){
             link_id: false,
             current_requester_phone: false
         });
-        console.log('Acceso correcto a todas las citas.');
+        console.log('Acceso correcto a los horarios.');
         return appointment
     }catch(err){
-        console.log('Error, no se pudo acceder a la cita.');
+        console.log('Error, no se pudo acceder a los horarios.');
     }
 }
 
 async function get_appointment_by_query_projection(query, projection){
     try{
+        await set_db_connection();
         const appointments = await Appointment.find(query, projection);
         console.log('Acceso correcto a las citas.');
         return appointments;
     }catch(err){
         console.log('Error, no se pudo acceder a las citas.');
     }
-} 
+}
+
+async function get_requester_schedules_by_fixer_month(fixer_id, requester_id, month){
+    try{
+        await set_db_connection();
+        const current_date = new Date(Date.now());
+        const current_year = current_date.getFullYear();
+        const target_month = month - 1;
+        const start_date = new Date(current_year, target_month, 1);
+        const finish_date = new Date(current_year, month, 0, 23, 59, 59);
+        const schedules = await Appointment.find({
+            id_fixer: fixer_id,
+            id_requester: requester_id,
+            selected_date: {
+                $gte: start_date,
+                $lte: finish_date
+            }
+        },
+        {
+            id_fixer: false,
+            id_requester: false,
+            selected_date: false,
+            selected_date_state: false,
+            current_requester_name: false,
+            appointment_type: false,
+            appointment_description: false,
+            place_id: false,
+            link_id: false,
+            current_requester_phone: false
+        });
+        console.log('Acceso correcto a los horarios.');
+        return schedules;
+    }catch(err){
+        console.log('Error, no se pudo acceder a los horarios');
+    }
+}
 
 module.exports = {
     get_all_locations,
@@ -126,6 +162,7 @@ module.exports = {
     get_location_by_place_id,
     get_locations_by_query_projection,
     get_all_appointments,
-    get_schedules_by_fixer_month,
-    get_appointment_by_query_projection
+    get_all_requester_schedules_by_fixer_month,
+    get_appointment_by_query_projection,
+    get_requester_schedules_by_fixer_month
 }

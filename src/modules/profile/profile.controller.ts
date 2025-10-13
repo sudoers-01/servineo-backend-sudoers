e4c8078038a4017d7d0967cecdbc800467df20be
GianniRo05 <gianni.jaimes.2005@gmail.com>
2025-10-13 00:41:46 -0400

    feat: calculate average rating of fixers

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -1,19 +1,47 @@
 import { Request, Response } from 'express';
+import { getDB as db } from '../../config/db/mongoClient';
+import { ObjectId } from 'mongodb';
+import { getAllFixersAverageRatings } from './profile.service';
 
-export function getProfileData(_req: Request, res: Response) {
-  setTimeout(() => {
+export async function getProfileData(req: Request, res: Response) {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ error: 'Missing user ID' });
+  }
+  if (!(ObjectId.isValid(id) && new ObjectId(id).toHexString() === id)) {
+    return res.status(422).json({ error: 'Invalid user ID format' });
+  }
+
+  try {
+    const usersCollection = db().collection('users');
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
     return res.status(200).json({
-      name: 'Juan Condori Quispe',
-      photo_url:
-        'https://www.shutterstock.com/image-photo/bold-portrait-photo-40yearold-latino-260nw-2627618737.jpg',
-      role: 'Carpintero',
-      average_rating: 1.36,
-      ratings: {
-        1: 15,
-        2: 2,
-        3: 2,
-      },
-      rating_count: 10,
+      name: user.name,
+      photo_url: user.url_photo,
+      role: user.role,
+      //TODO: Gianni - Replace with real ratings data when available
+      average_rating: user.average_rating || 1.5,
+      ratings: user.ratings || { 1: 14, 2: 4, 3: 1 },
+      rating_count: user.rating_count || 19,
     });
-  }, 1500);
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+export async function getAllFixersAverage(_req: Request, res: Response) {
+  try {
+    const data = await getAllFixersAverageRatings();
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching all fixers averages:', error);
+    return res.status(500).json({ message: 'Error fetching all fixers averages' });
+  }
 }
--- src/modules/profile/profile.routes.ts
+++ src/modules/profile/profile.routes.ts
@@ -3,6 +3,8 @@ import * as ProfileController from './profile.controller';
 
 const router = Router();
 
-router.get('/profile', ProfileController.getProfileData);
+router.get('/profile/:Id', ProfileController.getProfileData);
+
+router.get('/averageFixers', ProfileController.getAllFixersAverage);
 
 export default router;

3d05e2a34b81b59c6b28d5486f507d0b09ae909f
gg1700 <gabrielcamacho1700@gmail.com>
2025-10-12 21:12:47 -0400

    Reestructuration.

---
+++ src/modules/CRUD_operations/update_service.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+
+const dbConnection = require('../../database');
+
+dbConnection();
+
+const Location = require('../../models/Location');
+
+//Ubicaciones
+async function update_location_fields_by_display_name(name, attributes){
+    try{
+        const location = await Location.findOneAndUpdate({display_name: name}, {$set: attributes}, {new: true});
+        console.log('Atributos de localizacion modificado.');
+        return location;
+    }catch(err){
+        console.log('Error, no se pudo modificar la ubicacion: ', err);
+        throw err;
+    }
+}
+
+async function update_many_locations_fields_by_display_name(names, attributes){
+    try{
+        const locations = await Location.updateMany({display_name: names}, {$set: attributes});
+        console.log('Atributos de las localizaciones modificados.');
+        return locations;
+    }catch(err){
+        console.log('Error, no se pudo modificar las ubicaciones: ', err);
+        throw err;
+    }
+}
+
+async function update_location_fields_by_place_id(id, attributes){
+    try{
+        const location = await Location.findOneAndUpdate({place_id: id}, {$set: attributes}, {new: true});
+        console.log('Atributos de ubicacion modificados.');
+        return location;
+    }catch(err){
+        console.log('Error, no se pudo modificar la ubicacion: ', err);
+        throw err;
+    }
+}
+
+async function update_many_locations_fields_by_place_id(ids, attributes){
+    try{
+        const locations = await Location.updateMany({place_id: ids}, {$set: attributes});
+        console.log('Atributos de las ubicaciones modificados.');
+        return locations;
+    }catch(err){
+        console.log('Error, no se pudo modificar las ubicaciones: ', err);
+        throw err;
+    }
+}
+
+async function update_many_locations_fields_by_query(query, attributes){
+    try{
+        const locations = await Location.updateMany(query, {$set: attributes});
+        console.log('Atributos de las ubicaciones modificados.');
+        return locations;
+    }catch(err){
+        console.log('Error, no se pudo modificar las ubicaciones: ', err);
+        throw err;
+    }
+}
+
+async function update_all_locations_fields(attributes){
+    try{
+        const locations = await Location.updateMany({}, {$set: attributes});
+        console.log('Atributos de las localizaciones modificados.');
+        return locations;
+    }catch(err){
+        console.log('Error, no se pudo modificar las ubicaciones: ', err);
+        throw err;
+    }
+}
+
+//Citas:
+async function update_appointment_fields_by_display_name(){
+    
+}
+
+module.exports = {
+    update_location_fields_by_display_name,
+    update_many_locations_fields_by_display_name,
+    update_location_fields_by_place_id,
+    update_many_locations_fields_by_place_id,
+    update_all_locations_fields,
+    update_many_locations_fields_by_query
+}

51dfd060fa66a7e67ca3bff8c1fe7c960413788a
MiguelQuiton <202201690@est.umss.edu>
2025-10-19 13:15:02 -0400

    Fix(db):arreglar conexion

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ dotenv.config({ path: '.env.local' });
 
 import googleRouter from './modules/controlC/google/routes';
 import ubicacionRouter from './modules/controlC/ubicacion/routes';
-// Asumo que 'registrarDatosRouter' es el router que contiene la ruta de registro manual
+
 import registrarDatosRouter from './modules/controlC/registrarDatos/routes';
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 
-// RUTAS CORREGIDAS
+
 app.use('/api/controlC/google', googleRouter);
 app.use('/api/controlC/ubicacion', ubicacionRouter);
-// 🛑 CORREGIDO: Cambiamos el prefijo de 'registrarDatos' a 'registro'
+
 app.use('/api/controlC/registro', registrarDatosRouter);
 
 app.listen(8000, () => console.log('Servidor corriendo en puerto 8000'));

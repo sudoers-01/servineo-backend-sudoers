6ec90f1b8f8c3b0c2812b0bfcde431e9a12295ae
alex-opt-21 <202010391@est.umss.edu>
2025-10-20 23:36:53 -0400

    Añadir url del fronted

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import modificarDatosRouter from './modules/controlC/HU5/modificarDatos/routes';
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://fronted-pearl.vercel.app", credentials: true }));
 app.use(express.json());
 
 
--- src/modules/health/health.routes.ts
+++ src/modules/health/health.routes.ts
@@ -1,10 +1,8 @@
-// src/modules/health/health.routes.ts
 import { Router } from 'express';
 import { getHealth } from './health.controller';
 
 const router = Router();
 
-// Ruta GET /api/health
 router.get('/health', getHealth);
 
 export default router; 

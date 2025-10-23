09babf2c7817946c554659a1441ee454193ef8a4
Luis Gustavo <luisgustavogarcia576@gmail.com>
2025-10-23 14:55:14 -0400

    numero ya registrado funcion: generador error 409

--- src/modules/controlC/HU5/modificarDatos/controller.ts
+++ src/modules/controlC/HU5/modificarDatos/controller.ts
@@ -25,6 +25,18 @@ export const actualizarDatosUsuario = async (req: Request, res: Response) => {
     const result = await actualizarDatosUsuarioService(token, nuevosDatos);
     res.json(result);
   } catch (error: any) {
+    // erro del manejo especifico para telefono duplicado o ya registrado
+    if ((error as any).code=="PHONE_TAKEN"){
+      return res.status(409).json({
+            success:false,
+            error:"PHONE_TAKEN",
+            message:"El numero de telefono ya esta registrado por otro usuario",          
+      });
+      //por lo tanto da error 409 cuando ya existe el numero
+    }
+
+    //sino por defecto el error 400
     res.status(400).json({ message: error.message });
+    
   }
 };

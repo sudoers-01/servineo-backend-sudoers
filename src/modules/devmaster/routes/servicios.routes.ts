da4d28abe16fa91aca1863106481186eeaa7055c
John Ivan Sinchi Nina <johnivansn@gmail.com>
2025-10-28 23:35:01 -0400

    chore: eliminar archivos innecesarios

--- src/modules/devmaster/routes/servicios.routes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/servicios.routes.ts
-import { Router } from 'express';
-import { getServiciosByName } from '../controller/servicios.controller';
-
-const router = Router();
-
-router.get('/servicios', getServiciosByName);
-
-export default router;
--- src/modules/devmaster/routes/sort.routes.ts
+++
@@ -1,9 +0,0 @@
-// src/modules/devmaster/routes/sort.routes.ts
-import { Router } from 'express';
-import { getFixers } from '../controller/sort.controller';
-
-const router = Router();
-
-router.get('/fixers', getFixers);
-
-export default router;

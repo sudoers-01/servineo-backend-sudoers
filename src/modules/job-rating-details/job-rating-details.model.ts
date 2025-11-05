2e4349f2ff1d98e6f6142bc0c33573799a568cea
GianniRo05 <gianni.jaimes.2005@gmail.com>
2025-11-04 22:01:26 -0400

    Add: job-rating.details.service.ts

---
+++ src/modules/job-rating-details/job-rating-details.model.ts
@@ -0,0 +1,9 @@
+export interface JobRatingDetails {
+  _id: string;
+  fixerId: string;
+  title: string;
+  description: string;
+  type: string;
+  rating: number;
+  createdAt: string;
+}
---
+++ src/modules/job-rating-details/job-rating-details.service.ts
@@ -0,0 +1,29 @@
+import { Db, ObjectId } from 'mongodb';
+import { JobRatingDetails } from './job-rating-details.model';
+
+export async function getJobRatingDetails(db: Db, jobId: ObjectId, fixerId: ObjectId): Promise<JobRatingDetails | null> {
+  const collection = db.collection('jobs');
+
+  if (!ObjectId.isValid(jobId)) {
+    throw new Error('Invalid job ID');
+  }
+
+  const job = await collection.findOne<JobRatingDetails>({
+    _id: jobId,
+    fixerId,
+    rating: { $exists: true },
+    status: { $regex: /^Pagado$/i },
+  });
+
+  if (!job) return null;
+
+  return {
+    _id: job._id.toString(),
+    fixerId: job.fixerId.toString(),
+    title: job.title,
+    description: job.description,
+    type: job.type,
+    rating: job.rating,
+    createdAt: job.createdAt,
+  };
+}

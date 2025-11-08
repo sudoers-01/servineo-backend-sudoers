1e002f6cdd5f97490b88b8f4bb19e789ab06d1e0
LucianaBarrosoF <202200291@est.umss.edu>
2025-11-08 19:18:00 -0400

    feat(auth): agregar ruta y configuración para login con GitHub

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import ubicacionRouter from "./modules/controlC/HU3/ubicacion/routes";
 import authRouter from "./modules/controlC/HU4/auth/auth.routes"; 
 import modificarDatosRouter from './modules/controlC/HU5/modificarDatos/routes';
 import nominatimRouter from './modules/controlC/HU5/sugerencias/routes'; // si lo separaste
+import githubAuthRouter from "./modules/controlC/HU7/routes";
 
 
 const app = express();
@@ -23,5 +24,6 @@ app.use("/api/controlC/auth", authRouter);
 app.use('/api/controlC/registro', registrarDatosRouter);
 app.use('/api/controlC/modificar-datos', modificarDatosRouter);
 app.use('/api/controlC/sugerencias', nominatimRouter);
+app.use("/auth", githubAuthRouter);
 
 app.listen(8000, () => console.log('Servidor corriendo en puerto 8000'));

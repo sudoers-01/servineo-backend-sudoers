4d6149a2eae2cc03ae35b277ea571b92410d3d7d
heidyMC <heidy20@gmail.com>
2025-11-08 15:49:20 -0400

    feat: arreglo de ultimos detalles del codigo

--- src/modules/rated-jobs/controller.ts
+++ src/modules/rated-jobs/controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { getDB } from '../../config/db/mongoClient'; // 👈 importante: usa la ruta correcta
+import { getDB } from '../../config/db/mongoClient'; // importante: usa la ruta correcta
 
 export const getRatedJobs = async (req: Request, res: Response) => {
   try {
     const db = getDB();
     const jobsCollection = db.collection('jobs');
 
-    // Traer todos los jobs que tienen rating
+    // Trae todos los jobs que tienen rating
     const ratedJobs = await jobsCollection.find({ rating: { $exists: true } }).toArray();
 
     // Filtrar solo los campos relevantes

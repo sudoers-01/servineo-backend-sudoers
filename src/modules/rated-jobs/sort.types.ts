8a094f7a4befd46a80705fe304ec45328c81c742
John Ivan Sinchi Nina <johnivansn@gmail.com>
2025-11-11 10:50:35 -0400

    feat(sort): añade configuración y criterios de ordenamiento

---
+++ src/modules/rated-jobs/sort.types.ts
@@ -0,0 +1,8 @@
+export enum SortCriteria {
+  DATE_RECENT = 'recent',
+  DATE_OLDEST = "oldest",
+  RATING_ASC = 'rating_asc',
+  RATING_DESC = 'rating_desc',
+}
+
+export const DEFAULT_SORT_CONFIG = SortCriteria.DATE_RECENT;

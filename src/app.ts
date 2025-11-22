fbcc3b6da979ab88122324eb3d9dcaf0c0d3904f
Freddy Amin Zapata <zapatamorato@gmail.com>
2025-11-22 14:36:08 -0300

    Updated back

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import HealthRoutes from './api/routes/health.routes';
 import jobOfertRoutes from './api/routes/jobOfert.routes';
 import newoffersRoutes from './api/routes/newOffers.routes';
 import fixerRoutes from './api/routes/fixer.routes';
+import userProfileRoutes from './routes/userProfile.routes';
+import jobRoutes from './routes/job.routes';
 
 const app = express();
 
 app.use(
   cors({
-    origin: ['https://devmasters-servineo-frontend-zk3q.vercel.app', 'http://localhost:8080'],
+    origin: ['http://localhost:3000', 'http://localhost:8080','*'],
     credentials: true,
   }),
 );
@@ -22,7 +24,8 @@ app.use('/api', HealthRoutes);
 app.use('/api/devmaster', jobOfertRoutes);
 app.use('/api/newOffers', newoffersRoutes);
 app.use('/api/fixers', fixerRoutes);
-
+app.use('/api/user-profiles', userProfileRoutes);
+app.use('/api/jobs', jobRoutes);
 // 404 handler
 app.use((req, res) => {
   console.log('Not found:', req.method, req.originalUrl);

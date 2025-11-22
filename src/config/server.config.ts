0bdefef9027f656f8e0904795fd9982051b904dc
Erick <202111304@est.umss.edu>
2025-11-22 09:47:35 -0400

    Update server.config.ts

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,1 +1,12 @@
-//
+import express from "express";
+import cors from "cors";
+import serviciosRoutes from "../routes/servicios.routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/newoffers", serviciosRoutes);
+
+export default app;

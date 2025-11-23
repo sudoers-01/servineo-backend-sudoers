7086588c1ee07bcf48ae5e57f91593faa7879325
LucianaBarrosoF <202200291@est.umss.edu>
2025-11-22 23:57:33 -0400

    feat: utilizar modelo para registro de usuarios con GitHub

--- src/services/userManagement/github.service.ts
+++ src/services/userManagement/github.service.ts
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import clientPromise from "../../config/db/mongodb";
 import { ObjectId } from "mongodb";
+import { IUser } from "../../models/requester.model";
 
 interface GitHubUser {
   email: string;
@@ -9,22 +10,17 @@ interface GitHubUser {
   githubId: number;
 }
 
-interface User extends GitHubUser {
-  authProviders: any;
-  _id: ObjectId;
-  role: string;
-  url_photo: string;
-}
-
 export async function getGitHubUser(accessToken: string): Promise<GitHubUser | null> {
   const userResp = await fetch("https://api.github.com/user", {
     headers: { Authorization: `token ${accessToken}` },
   });
+
   const userData = await userResp.json();
 
   const emailResp = await fetch("https://api.github.com/user/emails", {
     headers: { Authorization: `token ${accessToken}` },
   });
+
   const emails = await emailResp.json();
   const primaryEmail = emails.find((e: any) => e.primary)?.email;
 
@@ -38,36 +34,54 @@ export async function getGitHubUser(accessToken: string): Promise<GitHubUser | n
   };
 }
 
-export async function findUserByEmail(email: string): Promise<User | null> {
+export async function findUserByEmail(email: string) {
   const mongoClient = await clientPromise;
   const db = mongoClient.db("ServineoBD");
-  const user = await db.collection("users").findOne<User>({ email });
-  return user;
+
+  const user = await db.collection<IUser>("users").findOne({
+    "authProviders.provider": "github",
+    "authProviders.providerId": email,
+  });
+
+  if (!user) return null;
+  return { ...user, _id: user._id as ObjectId };
 }
 
-export async function createUser(user: GitHubUser): Promise<User> {
+export async function checkUserExists(email: string): Promise<boolean> {
+  const user = await findUserByEmail(email);
+  return !!user;
+}
+
+export async function createUser(githubUser: GitHubUser): Promise<IUser & { _id: ObjectId }> {
   const mongoClient = await clientPromise;
   const db = mongoClient.db("ServineoBD");
 
-  const newUserDocument = {
-    name: user.name,
-    email: user.email,
-    url_photo: user.picture || "",
+  const newUser: IUser = {
+    name: githubUser.name,
+    email: githubUser.email,
+    url_photo: githubUser.picture || "",
     role: "requester",
-    especialidad: "",
+
+    authProviders: [
+      {
+        provider: "github",
+        providerId: githubUser.email,
+        password: "",
+      },
+    ],
+
     telefono: "",
-    certificacion: "",
-    language: "es",
-    authProviders: [{ provider: "github", email: user.email, githubId: user.githubId }],
-    createdAt: new Date(),
+    servicios: [],
+    ubicacion: {},
+    ci: "",
+    vehiculo: {},
+    acceptTerms: false,
+    metodoPago: {},
+    experience: {},
+    workLocation: {},
   };
 
-  const result = await db.collection("users").insertOne(newUserDocument);
-  console.log("Usuario insertado en MongoDB:", user.email);
+  const result = await db.collection<IUser>("users").insertOne(newUser);
 
-  return {
-    _id: result.insertedId,
-    ...user,
-    ...newUserDocument,
-  } as unknown as User;
+  return { ...newUser, _id: result.insertedId };
 }

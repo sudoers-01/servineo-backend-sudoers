e39e63c9fa23dd5d5f6f02967278da2593d26a74
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-12-02 02:32:19 -0400

    feat: add post metods to work with frontend

--- src/config/server.routes.ts
+++ src/config/server.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import HealthRoutes from '../api/routes/health.routes';
 
 import AuthRoutes from '../api/routes/userManagement/auth.routes';
+import offerUpdateRouter from '../api/routes/offerUpdate.routes';
 
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 // Todas las rutas de health bajo /api
 router.use('/api', HealthRoutes);
 router.use('/api/auth', AuthRoutes);
+router.use('/api/offers', offerUpdateRouter);
 
 
 // Manejo de rutas no encontradas
